c8b8d53a61ec1995f70d88095d3a74c66c1c49a0
李涛 <864373801@qq.com>
2018-12-05 14:52:54 +0800

    获得授权码和accesstoken

--- helloweibo/helloweibo/New Group/Tools/NetworkTools.swift
+++ helloweibo/helloweibo/New Group/Tools/NetworkTools.swift
@@ -15,16 +15,42 @@ enum WBHTTPMethod {
 }
 //MARK: -网络工具
 class NetworkTools: AFHTTPSessionManager {
-
+    //MARK: - 应用程序信息
+    private var appKey = "69271884"   //clinet  id
+    private var appSecret = "8ebcd4209f23bf9026724ac333e04f68"
+    private var redirectUrl = "http://www.baidu.com"
+    //类似于OC 的typeDefine
+    typealias HMResquestCallBack = (_ json: AnyObject?,_ isSuccess: Bool) ->Void
     //建立单例
     static let sharedTools:NetworkTools = {
         let tools = NetworkTools(baseURL:nil)
         //tools.requestSerializer.acceptableContentTypes?.insert("text/heml")
         return tools
     }()
-    
-    
 }
+extension NetworkTools{
+    ///OAuth授权
+    ///- see:[https://api.weibo.com/oauth2/authorize](http://open.weibo.com/wiki/Oauth2/authorize)
+    var oauthURL:NSURL{
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(appKey)&redirect_uri=\(redirectUrl)"
+        return NSURL(string:urlString)!
+    }
+    
+    ///加载AccessToken
+    func loadAccessToken(code:String,completion:@escaping HMResquestCallBack) {
+        
+        let urlString = "https://api.weibo.com/oauth2/access_token"
+        let params = ["client_id":appKey,
+        "client_secret":appSecret,
+        "grant_type":"authorization_code",
+        "code":code,
+        "redirect_uri":redirectUrl]
+   
+        request(method: WBHTTPMethod.POST, URLString: urlString, parameters:params as [String : AnyObject], completion: completion)
+        }
+    }
+
+
 
 
 //MARK: -封装AFN网络方法
@@ -35,7 +61,7 @@ extension NetworkTools {
     /// - parameter URLString:  URLString
     /// - parameter parameters: 参数字典
     /// - parameter completion: 回调json、是否成功
-    func request(method: WBHTTPMethod = .GET, URLString: String, parameters: [String:AnyObject]?, completion: @escaping (_ json: AnyObject?,_ isSuccess: Bool) ->Void) {
+   private func request(method: WBHTTPMethod = .GET, URLString: String, parameters: [String:AnyObject]?, completion: @escaping HMResquestCallBack) {
         
         //成功回调
         let success = {(task: URLSessionDataTask, json: Any?) ->() in
--- helloweibo/helloweibo/New Group/View/OAuth/OAuthViewController.swift
+++ helloweibo/helloweibo/New Group/View/OAuth/OAuthViewController.swift
@@ -9,28 +9,67 @@
 import UIKit
 
 class OAuthViewController: UIViewController {
-
+    private lazy var webView = UIWebView()
+    //MARK: -监听方法
+    @objc private func close(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    ///自动填充用户名和密码  -web 注入
+    @objc private func autoFill(){
+        let js = "document.getElementById('userId').value = '13093627213';" +
+        "document.getElementById('passwd').value = 'Taylor19891213x'"
+        webView.stringByEvaluatingJavaScript(from: js)
+    }
+    //MARK: -设置界面
+    override func loadView() {
+        view = webView
+        //设置代理
+        webView.delegate = self
+        //设置导航栏
+        title = "登录新浪微博"
+        navigationItem.leftBarButtonItem =  UIBarButtonItem(title: "关闭", style: .plain, target: self, action: #selector(close))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "自动填充", style: .plain, target: self, action: #selector(autoFill))
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
+       //view.backgroundColor = UIColor.white
+        //加载页面
+        self.webView.loadRequest(NSURLRequest(url: NetworkTools.sharedTools.oauthURL as URL) as URLRequest)
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+//MARK: -UIWebViewDelegate
+extension OAuthViewController:UIWebViewDelegate{
+    //将要加载的请求方法
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        //如果是百度就不加载
+        guard let url = request.url, url.host == "www.baidu.com" else{
+            return true
+        }
+        //2从百度地址提取code=是否存在
+        guard let query = url.query,query.hasPrefix("code=") else {
+            print("取消授权")
+            return false
+        }
+        //3从query字符串中提取code=后面的授权码
+        let code = String(query["code=".endIndex...])
+        print(query)
+        print("授权码是" + code)
+        //4  加载accessToken
+        NetworkTools.sharedTools.loadAccessToken(code: code) { (result, success) in
+            if success==nil{
+               print("出错了")
+            }
+            print(result!)
+            return
+        }
+        
+        return false
+     
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
